dbe114d5d7a5f5aea99ff7fce7a731fd9250635b
Jansenck <caik11@hotmail.com>
2023-04-10 20:38:36 -0300

    feat: create users

--- src/main/java/com/tweteroo/api/controllers/SignupController.java
+++ src/main/java/com/tweteroo/api/controllers/SignupController.java
@@ -1,16 +1,30 @@
 package com.tweteroo.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tweteroo.api.dto.PersonDTO;
+import com.tweteroo.api.model.Person;
+import com.tweteroo.api.repositories.PersonRepository;
+
 @RestController
 @RequestMapping("/api/users")
 public class SignupController {
+
+    @Autowired
+    private PersonRepository repository;
+
+    @GetMapping
+    public void listAll() {
+        System.out.println(repository.findAll());
+    }
     
     @PostMapping
-    public void create(@RequestBody String req) {
-        System.out.println("Tá indo, hein...");
+    public void create(@RequestBody PersonDTO req) {
+        repository.save(new Person(req));
     }
 }
